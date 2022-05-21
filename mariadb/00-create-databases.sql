f6dd9ebc75c459182727ebbe40557a8016bc62a2
m.nozili <m.nozili@fintechsys.net>
2022-05-21 10:18:58 +0300

    adding utf-8 to db creation

--- mariadb/00-create-databases.sql
+++ mariadb/00-create-databases.sql
@@ -1,2 +1,5 @@
-create database `mifosplatform-tenants`;
+create database `mifosplatform-tenants`
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
 create database `mifostenant-default`;
